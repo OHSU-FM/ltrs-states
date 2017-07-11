e191832e802d4dd36496bfc91fd8e12022a99446
Pema Choephel <choephel@ohsu.edu>
2017-07-11 16:18:07 -0700

    wip travel request view

--- app/helpers/travel_requests_helper.rb
+++ app/helpers/travel_requests_helper.rb
@@ -1,2 +1,11 @@
 module TravelRequestsHelper
+  def hf_funding_options
+  	[
+      ['N/A',[
+       'None of these - Describe below',
+       'Department Funding - Org. 63601',
+       'Bridge Funding']
+      ]
+    ]
+  end
 end
