86721ef7a960d52c01d9158050163943c6c6f86e
William Eichelberger <wceichelb@gmail.com>
2017-09-04 16:42:03 -0700

    add server sender email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ltrs-states_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "fmresearch.ohsu.edu" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
