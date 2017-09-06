f3549f24df8dd3003c895dcaf8f536f432ce0566
William Eichelberger <wceichelb@gmail.com>
2017-09-06 15:22:04 -0700

    allow admin to view other users approvals list

--- app/controllers/users/approvals_controller.rb
+++ app/controllers/users/approvals_controller.rb
@@ -2,7 +2,7 @@ class Users::ApprovalsController < ApplicationController
   before_action :load_user
 
   def index
-    redirect_to root_path unless current_user == @user
+    redirect_to root_path unless (current_user.is_admin? || current_user == @user)
     reviewable = ApprovalSearch.by_params(@user, params)
     @approvals = WillPaginate::Collection.create(@page, @per_page, reviewable.length) do |pager|
       pager.replace reviewable
