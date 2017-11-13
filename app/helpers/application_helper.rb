47da47c1aa0a05d0a2d4f834ce20e0cc982ddec9
William Eichelberger <wceichelb@gmail.com>
2017-11-13 15:28:15 -0800

    move flash rendering to helper, fix weird lack of save button on edit rerender after error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,4 +53,14 @@ module ApplicationHelper
   def bootstrap_class_for key
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[key.to_sym] || key
   end
+
+  def flash_messages opts = {}
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
